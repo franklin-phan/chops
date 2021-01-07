3474910f8e6908a84fb1b6b68e736f20ea1e3939
FRANKLIN_PHAN <franklin.phan123@gmail.com>
2021-01-07 13:41:55 -0600

    hooked app to Firebase

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import firebase from 'firebase'
+
+const config = {
+    apiKey: "AIzaSyDl1h-YYooOKqWdOvpBoDMq1yFcoC1InCc",
+    authDomain: "chops-3f363.firebaseapp.com",
+    projectId: "chops-3f363",
+    storageBucket: "chops-3f363.appspot.com",
+    messagingSenderId: "117004854879",
+    appId: "1:117004854879:web:448bef686f5ed1d4721dea",
+    measurementId: "G-1WRXC3JY45"
+};
+
+firebase.initializeApp(config);
+export default firebase;
