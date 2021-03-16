111c0bbabeba4bcb9f403f110ed36a26c84d2f8b
Jonathan Warner <jewarner57@gmail.com>
2021-03-16 18:58:35 -0400

    feat: style signup form

--- src/SignUp.js
+++ src/SignUp.js
@@ -43,25 +43,28 @@ const SignUp = (props) => {
 
     return (
         <div>
-            <h1 class="auth-header">Sign up</h1>
+            <div class="centered-flex-row">
+                <h1 class="auth-header">Sign up</h1>
+                <button type="button" className="close-modal" onClick={props.cancel}>X</button>
+            </div>
             <form onSubmit={handleSignUp}>
                 <label>Name:
-                <input name="username" type="text" id="username" placeholder="John Doe" required />
+                <input name="username" type="text" id="username" required />
                 </label>
 
                 <label>Email:
-                <input name="email" type="email" id="email" placeholder="Email" required />
+                <input name="email" type="email" id="email" required />
                 </label>
 
                 <label>Password:
-                <input name="password" type="password" id="password" placeholder="New Password" value={password} required
+                <input name="password" type="password" id="password" value={password} required
                         onChange={(e) => {
                             setPassword(e.target.value)
                         }} />
                 </label>
 
                 <label>Confirm Password:
-                <input type="password" id="confirmpassword" placeholder="Repeat Password" value={confirmPassword} className={`${passwordsMatch ? "" : "inputError"}`} required
+                <input type="password" id="confirmpassword" value={confirmPassword} className={`${passwordsMatch ? "" : "inputError"}`} required
                         onChange={(e) => {
                             setConfirmPassword(e.target.value)
                         }} />
@@ -73,13 +76,13 @@ const SignUp = (props) => {
                     })}
                 </div>
 
-                <div className="centered-flex-column">
+                <div className="centered-flex-row">
                     <button type="submit">Sign Up</button>
-                    <div className="centered-flex-row">
-                        <button type="button" className="w-49" onClick={props.cancel}>Cancel</button>
-                        <button type="button" className="w-49" onClick={props.oauth}>
-                            {/* <img src={process.env.PUBLIC_URL + '/google-icon.svg'} class="inline-icon" /> */}
-                            Sign Up with Google</button>
+                    <div class="google-btn" onClick={props.oauth}>
+                        <div class="google-icon-wrapper">
+                            <img class="google-icon-svg" src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg" />
+                        </div>
+                        <p class="btn-text"><b>Sign in with Google</b></p>
                     </div>
                 </div>
 
