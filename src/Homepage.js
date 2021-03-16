6bd2c2d52348e0858d9b7813b8573f26b1bc9ba7
Jonathan Warner <jewarner57@gmail.com>
2021-03-16 11:04:18 -0400

    feat: add footer

--- src/Homepage.js
+++ src/Homepage.js
@@ -5,36 +5,41 @@ import SignInModal from './SignInModal'
 
 function Homepage() {
     return (
-        <div className='homepage-wrapper'>
-            <div className='hero-image-container'>
-                <div class="scene">
-                    <div class="vaporwave-container">
-                        <div class="sun"></div>
-                        <div class="band" style={{ animationDelay: "-0s" }}></div>
-                        <div class="band" style={{ animationDelay: "-1s" }}></div>
-                        <div class="band" style={{ animationDelay: "-2s" }}></div>
-                        <div class="band" style={{ animationDelay: "-3s" }}></div>
-                        <div class="band" style={{ animationDelay: "-4s" }}></div>
-                        <div class="band" style={{ animationDelay: "-5s" }}></div>
-                        <div class="band" style={{ animationDelay: "-6s" }}></div>
-                        <div class="band" style={{ animationDelay: "-7s" }}></div>
-                        <div class="band" style={{ animationDelay: "-8s" }}></div>
-                        <div class="band" style={{ animationDelay: "-9s" }}></div>
+        <section class="homepage-section">
+            <div className='homepage-wrapper'>
+                <div className='hero-image-container'>
+                    <div class="scene">
+                        <div class="vaporwave-container">
+                            <div class="sun"></div>
+                            <div class="band" style={{ animationDelay: "-0s" }}></div>
+                            <div class="band" style={{ animationDelay: "-1s" }}></div>
+                            <div class="band" style={{ animationDelay: "-2s" }}></div>
+                            <div class="band" style={{ animationDelay: "-3s" }}></div>
+                            <div class="band" style={{ animationDelay: "-4s" }}></div>
+                            <div class="band" style={{ animationDelay: "-5s" }}></div>
+                            <div class="band" style={{ animationDelay: "-6s" }}></div>
+                            <div class="band" style={{ animationDelay: "-7s" }}></div>
+                            <div class="band" style={{ animationDelay: "-8s" }}></div>
+                            <div class="band" style={{ animationDelay: "-9s" }}></div>
+                        </div>
                     </div>
                 </div>
-            </div>
-            <div className='hero-auth-container'>
-                <div className='hero-auth-page'>
-                    <p className="text-center header-text text-light">Jam Session</p>
-                    <p className="text-light slogan-text">Make More Than Just Music</p>
+                <div className='hero-auth-container'>
+                    <div className='hero-auth-page'>
+                        <p className="text-center header-text text-light">Jam Session</p>
+                        <p className="text-light slogan-text">Make More Than Just Music</p>
 
-                    <div className="auth-modal">
-                        <SignUpModal />
-                        <SignInModal />
+                        <div className="auth-modal">
+                            <SignUpModal />
+                            <SignInModal />
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
+            <div class="footer">
+                <div class="text-light">Copyright something or other</div>
+            </div>
+        </section>
 
     )
 }
