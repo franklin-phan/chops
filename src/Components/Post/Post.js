3a5456f30106de22cd01efda04ed16a0a0a1c6c6
Jonathan Warner <jewarner57@gmail.com>
2021-03-19 23:42:05 -0400

    feat: create post header

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -1,4 +1,4 @@
-import React , { useEffect, useState }from "react";
+import React, { useEffect, useState } from "react";
 import CommentInput from '../Comment/sendComment'
 import Comment from '../Comment/comment'
 import pfp from '../Comment/pfp.png'
@@ -10,80 +10,93 @@ import { selectUser } from '../../userRedux'
 
 import Time from '../../Time'
 function Post(data) {
-    const user = useSelector(selectUser);
-    console.log(user)
-    const [comments, setComments] = useState([])
-    const itemID = data.data.id
-    const {postedBy, snaps, songLink, timestamp, title} = data.data.data
-    const {email, displayName} = postedBy
-    console.log(timestamp)
-    useEffect(async () => {
-        const postCommentsRef = db.collection("posts").doc(itemID).collection("comments")
-        const res = await postCommentsRef.orderBy('timestamp', 'desc').onSnapshot(async snapshot => (
-        await setComments(snapshot.docs.map(doc => (
-            {
-              id: doc.id,
-              comment: doc.data(),
-            }
-          )))
-        ))
-    }, [])
-
-    function convertTimestamp(timestamp) {
-        if (!timestamp) {
-            return 'test'
+  const user = useSelector(selectUser);
+  console.log(user)
+  const [comments, setComments] = useState([])
+  const itemID = data.data.id
+  const { postedBy, snaps, songLink, timestamp, title } = data.data.data
+  const { email, displayName, photoUrl } = postedBy
+  console.log(timestamp)
+  useEffect(async () => {
+    const postCommentsRef = db.collection("posts").doc(itemID).collection("comments")
+    const res = await postCommentsRef.orderBy('timestamp', 'desc').onSnapshot(async snapshot => (
+      await setComments(snapshot.docs.map(doc => (
+        {
+          id: doc.id,
+          comment: doc.data(),
         }
-        console.log(timestamp)
-        // let date = timestamp.toDate();
-        // let mm = date.getMonth();
-        // let dd = date.getDate();
-        // let yyyy = date.getFullYear();
-        let date = Time(timestamp.toDate())
-        // console.log(date)
+      )))
+    ))
+  }, [])
 
-        // date = mm + '/' + dd + '/' + yyyy;
-        return date;
-      }
-    function removeItem() {
-        db.collection("posts").doc(itemID).delete().then(() => {
-            console.log("Document successfully deleted!");
-        }).catch((error) => {
-            console.error("Error removing document: ", error);
-        });
+  function convertTimestamp(timestamp) {
+    if (!timestamp) {
+      return 'test'
     }
+    console.log(timestamp)
+    // let date = timestamp.toDate();
+    // let mm = date.getMonth();
+    // let dd = date.getDate();
+    // let yyyy = date.getFullYear();
+    let date = Time(timestamp.toDate())
+    // console.log(date)
 
-    return (
-        <li key={itemID}>
-            <h3>{title}</h3>
-            {songLink.search("soundcloud") !== -1 ?
-                <iframe title="post"width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
-                src={"https://w.soundcloud.com/player/?url="+ songLink +"&am;"}>
-                </iframe> : null}
-            {songLink.search("spotify") !== -1 ? 
-                <iframe src={(songLink).replace("track", "embed/track")} title="post" width="100%" height="100%" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> : null}
-            {songLink.search("youtube") !== -1 ?
-                <iframe title="post" width="100%" height="166" src={(songLink).replace("watch?v=", "embed/")} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> : null}
-            <p>Posted by: {displayName}
-                {displayName === user.displayName || email === user.email ?
-                <button onClick={() => removeItem(itemID)}>Remove Item</button> : null}
-            </p>
-            {/* Profile Picture */}
-            <img src={pfp} width="30px" height="30px"/>
-            {/* Time Posted */}
-            <time>{convertTimestamp(timestamp)}</time>
-            {/* Snaps */}
-            <Snap snaps={snaps} itemID={itemID} userID={user.uid}/>
-            {/* Comment Form */}
-            <CommentInput itemID={itemID} userID={user.uid} user={user.displayName}/>
+    // date = mm + '/' + dd + '/' + yyyy;
+    return date;
+  }
+  function removeItem() {
+    db.collection("posts").doc(itemID).delete().then(() => {
+      console.log("Document successfully deleted!");
+    }).catch((error) => {
+      console.error("Error removing document: ", error);
+    });
+  }
 
-            {/* Comments if not undefined */}
-            {comments.map((comment) => {
-                console.log(comment)
-                  return (
-                    <Comment data={comment} itemID={itemID}/>
-                  )
-                })}
-            {/* {item.item.comments != undefined ?
+  return (
+    <li key={itemID} className="post-container">
+
+      <div className="post-header">
+        <div className="user-profile">
+          <img src={photoUrl} class="" />
+        </div>
+        <div className="centered-flex-column">
+          <p className="post-author">{displayName}</p>
+          <time>{convertTimestamp(timestamp)}</time>
+        </div>
+      </div>
+
+      <hr></hr>
+
+      <h3>{title}</h3>
+      {songLink.search("soundcloud") !== -1 ?
+        <iframe title="post" width="100%" height="166" scrolling="no" frameborder="no" allow="autoplay"
+          src={"https://w.soundcloud.com/player/?url=" + songLink + "&am;"}>
+        </iframe> : null}
+      {songLink.search("spotify") !== -1 ?
+        <iframe src={(songLink).replace("track", "embed/track")} title="post" width="100%" height="100%" frameborder="0" allowtransparency="true" allow="encrypted-media"></iframe> : null}
+      {songLink.search("youtube") !== -1 ?
+        <iframe title="post" width="100%" height="166" src={(songLink).replace("watch?v=", "embed/")} frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe> : null}
+      <p>Posted by: {displayName}
+        {displayName === user.displayName || email === user.email ?
+          <button onClick={() => removeItem(itemID)}>Remove Item</button> : null}
+      </p>
+      {/* Profile Picture */}
+      <img src={pfp} width="30px" height="30px" />
+      {/* Time Posted */}
+      <time>{convertTimestamp(timestamp)}</time>
+      {/* Snaps */}
+      <Snap snaps={snaps} itemID={itemID} userID={user.uid} />
+      {/* Comment Form */}
+      <CommentInput itemID={itemID} userID={user.uid} user={user.displayName} />
+
+      {/* Comments if not undefined */}
+      {comments.map((comment) => {
+        console.log(comment)
+        return (
+          <Comment data={comment} itemID={itemID} />
+        )
+      })}
+      {/* {item.item.comments != undefined ?
                 item.item.comments.map((comment) => {
                     return (
                         <div>
@@ -93,8 +106,8 @@ function Post(data) {
                         </div>
                     )
             }) : null} */}
-        </li>
-    );
+    </li>
+  );
 }
 
 export default Post;
