022b02150dc55f97639130a8e5add55807560206
Jonathan Warner <jewarner57@gmail.com>
2021-03-21 15:42:21 -0400

    fix: spotify widget overflow

--- src/Components/Post/Post.js
+++ src/Components/Post/Post.js
@@ -12,25 +12,25 @@ import { userLoggedIn, userIsOwner } from '../Authentication/IsLoggedIn'
 
 import Time from '../../Time'
 function Post(data) {
-    const user = useSelector(selectUser);
-    console.log(user)
-    const [comments, setComments] = useState([])
-    const [showComments, setShowComments] = useState(false)
-    const itemID = data.data.id
-    const {postedBy, snaps, songLink, timestamp, title} = data.data.data
-    const {email, displayName, uid, photoUrl} = postedBy
-    console.log(timestamp)
-    useEffect(async () => {
-        const postCommentsRef = db.collection("posts").doc(itemID).collection("comments")
-        const res = await postCommentsRef.orderBy('timestamp', 'desc').onSnapshot(async snapshot => (
-        await setComments(snapshot.docs.map(doc => (
-            {
-              id: doc.id,
-              comment: doc.data(),
-            }
-          )))
-        ))
-    }, [])
+  const user = useSelector(selectUser);
+  console.log(user)
+  const [comments, setComments] = useState([])
+  const [showComments, setShowComments] = useState(false)
+  const itemID = data.data.id
+  const { postedBy, snaps, songLink, timestamp, title } = data.data.data
+  const { email, displayName, uid, photoUrl } = postedBy
+  console.log(timestamp)
+  useEffect(async () => {
+    const postCommentsRef = db.collection("posts").doc(itemID).collection("comments")
+    const res = await postCommentsRef.orderBy('timestamp', 'desc').onSnapshot(async snapshot => (
+      await setComments(snapshot.docs.map(doc => (
+        {
+          id: doc.id,
+          comment: doc.data(),
+        }
+      )))
+    ))
+  }, [])
 
   function convertTimestamp(timestamp) {
     if (!timestamp) {
@@ -70,46 +70,46 @@ function Post(data) {
           <p className="post-author">{displayName}</p>
           <time>{convertTimestamp(timestamp)}</time>
         </div>
-          {userIsOwner(user, uid) ?
-                <button className="delete-post-button" onClick={() => removeItem(itemID)}>Remove Item</button> : null}
+        {userIsOwner(user, uid) ?
+          <button className="delete-post-button" onClick={() => removeItem(itemID)}>Delete Post</button> : null}
       </div>
 
       <hr style={{ margin: "0 20px" }}></hr>
       <p>{title}</p>
 
-      {/* Media Display */}
-      {songLink.search("soundcloud") !== -1 ?
-        <iframe title="post" width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay"
-          src={"https://w.soundcloud.com/player/?url=" + songLink + "&am;"}>
-        </iframe> : null}
-      {songLink.search("spotify") !== -1 ?
-        <iframe src={(songLink).replace("track", "embed/track")} title="post" width="100%" height="100%" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe> : null}
-      {songLink.search("youtube") !== -1 ?
-        <iframe title="post" width="100%" height="166" src={(songLink).replace("watch?v=", "embed/")} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> : null}
-      <div>
 
+      {/* Media Display */}
+      <div className="post-media">
+        {songLink.search("soundcloud") !== -1 ?
+          <iframe title="post" width="100%" height="166" scrolling="no" frameBorder="no" allow="autoplay"
+            src={"https://w.soundcloud.com/player/?url=" + songLink + "&am;"}>
+          </iframe> : null}
+        {songLink.search("spotify") !== -1 ?
+          <iframe src={(songLink).replace("track", "embed/track")} title="post" width="100%" height="100%" frameBorder="0" allowtransparency="true" allow="encrypted-media"></iframe> : null}
+        {songLink.search("youtube") !== -1 ?
+          <iframe title="post" width="100%" height="166" src={(songLink).replace("watch?v=", "embed/")} frameBorder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowFullScreen></iframe> : null}
       </div>
 
       <div className="post-actions-container">
         {/* Snaps */}
-        <Snap snaps={snaps} itemID={itemID} user={user} isLoggedIn={userLoggedIn(user)}/>
+        <Snap snaps={snaps} itemID={itemID} user={user} isLoggedIn={userLoggedIn(user)} />
         <CommentControls commentToggle={toggleComments} commentCount={comments.length} isActive={showComments} />
       </div>
 
       {/* Comments if not undefined */}
       {comments.map((comment, index) => {
         return showComments ? (
-          <Comment data={comment} itemID={itemID} key={index + itemID} isLoggedIn={userLoggedIn(user)}/>
+          <Comment data={comment} itemID={itemID} key={index + itemID} isLoggedIn={userLoggedIn(user)} />
         ) : null
       })}
 
       <div>{showComments ?
         /* Comment Form */
-        <CommentInput itemID={itemID} user={user} hidden={false} isLoggedIn={userLoggedIn(user)}/>
+        <CommentInput itemID={itemID} user={user} hidden={false} isLoggedIn={userLoggedIn(user)} />
         : null}
       </div>
 
-   </li>
+    </li>
   );
 }
 
