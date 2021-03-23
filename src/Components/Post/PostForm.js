a00b36a42bd5e4b66200b68faee555cc702243ed
Jonathan Warner <jewarner57@gmail.com>
2021-03-22 22:26:59 -0400

    feat: start styling create post

--- src/Components/Post/PostForm.js
+++ src/Components/Post/PostForm.js
@@ -1,18 +1,19 @@
 import React from 'react'
 
 export default function PostForm(props) {
-    const title = props.title
-    const songLink = props.songLink
-    const changeTitle = props.changeTitle
-    const changeSongLink = props.changeSongLink
-    const handleSubmit = props.handleSubmit
-    return (
-        <section>
-            <form onSubmit={handleSubmit}>
-                <input type="text" name="currentItem" placeholder="Describe your music" onChange={changeTitle} value={title} />
-                <input type="text" name="linkValue" placeholder="SoundCloud, Spotify, or YouTube Link" onChange={changeSongLink} value={songLink} />
-                <button>Post</button>
-            </form>
-        </section>
-    )
+  const title = props.title
+  const songLink = props.songLink
+  const changeTitle = props.changeTitle
+  const changeSongLink = props.changeSongLink
+  const handleSubmit = props.handleSubmit
+  return (
+    <section>
+      <form onSubmit={handleSubmit} className="createPostForm">
+        <legend>Create a post</legend>
+        <input type="text" name="currentItem" placeholder="Describe your music" onChange={changeTitle} value={title} />
+        <input type="text" name="linkValue" placeholder="SoundCloud, Spotify, or YouTube Link" onChange={changeSongLink} value={songLink} />
+        <button>Post</button>
+      </form>
+    </section>
+  )
 }
